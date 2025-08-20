3679e32cb15654f2b30d552e64f48d3a27f2a7a4
Raj Yadav <289raj@gmail.com>
2025-08-21 02:02:01 +0530

    redirect the short url to site Get Route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { ConnectToMongoDB} = require("./connect");
+const URL = require("./models/url");
 const app = express();
 const port = 8001;
 ConnectToMongoDB('mongodb://localhost:27017/short-url')
@@ -13,6 +14,20 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry  = await URL.findOneAndUpdate(
+        {
+            shortId
+        }, {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+    });
+    res.redirect(entry.redirectURL);
+});
 
 app.listen(port, () => {
     console.log("Server Started");
