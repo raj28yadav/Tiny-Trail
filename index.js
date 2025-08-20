3a3c38f3cbc1ef7382b1991523b36b3f187e9ac7
Raj Yadav <289raj@gmail.com>
2025-08-21 01:50:20 +0530

    mongodb connection and post route for short id creation

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const urlRoute = require("./routes/url");
+const { ConnectToMongoDB} = require("./connect");
 const app = express();
 const port = 8001;
+ConnectToMongoDB('mongodb://localhost:27017/short-url')
+.then(() => {
+    console.log("MongoDB Connected");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use("/url", urlRoute);
 
 app.listen(port, () => {
     console.log("Server Started");
