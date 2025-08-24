ec867879278bcf027c31892ba74e07959b37906d
Raj Yadav <289raj@gmail.com>
2025-08-24 23:48:18 +0530

    Header authentication by Bearer Authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,15 @@
 const { getUser} = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.cookies?.uid;
+//    const userUid = req.cookies?.uid;
+    const userUid = req.headers["authorization"];
 
     if(!userUid){
         return res.redirect("/login");
     }
-    const user = getUser(userUid);
+    const token = userUid.split('Bearer ')[1];
+    const user = getUser(token);
+    //const user = getUser(userUid);
     if(!user){
         return res.redirect("/login");
     }
@@ -14,8 +17,11 @@ async function restrictToLoggedinUserOnly(req, res, next) {
     next();
 }
 async function checkAuth(req, res, next) {
-    const userUid = req.cookies?.uid;
-    const user = getUser(userUid);
+//    const userUid = req.cookies?.uid;
+//    const user = getUser(userUid);
+    const userUid = req.headers["authorization"];
+    const token = userUid.split('Bearer ')[1];
+    const user = getUser(token);
     req.user = user;
     next();
 }
